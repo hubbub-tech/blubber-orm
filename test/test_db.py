cfce8c48edc33bb738d987d2294be5e1f085badc
Ade Balogun <abalogun316@gmail.com>
2021-04-18 09:58:03 +0100

    building out testing

---
+++ test/test_db.py
@@ -0,0 +1,7 @@
+# from blubber_orm.db import parse_uri, DatabaseConnection
+#
+# connection_uri = os.environ.get("DATABASE_URL")
+# print(parse_uri(connection_uri))
+import sys
+
+print(sys.modules)

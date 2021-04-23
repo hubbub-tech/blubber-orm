7384b79b4276361b3006b2fce1072893372ab643
Ade Balogun <abalogun316@gmail.com>
2021-04-23 12:28:31 +0100

    testing the models

---
+++ tests/test_models.py
@@ -0,0 +1,13 @@
+#import pytest
+import unittest
+import src.blubber_orm.db as db
+
+class TestDecorators(unittest.TestCase):
+    #Testing for item decorator and user decorator
+    pass
+
+class TestUsers(unittest.TestCase):
+    pass
+
+class TestItems(unittest.TestCase):
+    pass

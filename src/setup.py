2eed990bafb7cbb2f5450dd129f013e9a1a0f0a1
Ade Balogun <abalogun316@gmail.com>
2021-04-28 23:48:42 +0100

    instructions for updating package commented

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blubber_orm',
-      version='0.3',
+      version='0.3.1',
       description='Custom ORM for Hubbub DB',
       url='https://github.com/hubbub-tech/blubber-orm',
       author='Ade Balogun',
@@ -10,3 +10,9 @@ setup(name='blubber_orm',
       packages=find_packages(),
       install_requires=['psycopg2', 'datetime', 'uritools', 'pytz', 'Werkzeug']
       )
+
+#source .env
+#python3 setup.py sdist bdist_wheel
+#deactivate
+#python3 -m twine upload --repository testpypi dist/*
+#pip3 install  -i https://test.pypi.org/simple/ blubber-orm [--upgrade]

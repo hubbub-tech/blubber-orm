925984de7be88a74e234be99e0426d7fb44dc9ad
Ade Balogun <abalogun316@gmail.com>
2021-07-16 10:19:28 +0100

    next upgrade

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blubber_orm',
-      version='0.4.2',
+      version='0.5.0',
       description='Custom ORM for Hubbub DB',
       url='https://github.com/hubbub-tech/blubber-orm',
       author='Ade Balogun',

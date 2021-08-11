faae6a0dca72b0256d148cf450d59e66624c1d0b
Ade Balogun <abalogun316@gmail.com>
2021-08-11 10:41:16 +0100

    update blubber: complete dropoff and pickup

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='blubber_orm',
-      version='0.5.5',
+      version='0.5.6',
       description='Custom ORM for Hubbub DB',
       url='https://github.com/hubbub-tech/blubber-orm',
       author='Ade Balogun',

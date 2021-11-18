08088647fcafda8b87fe3b3e942550a2da963ab8
Ade Balogun <abalogun316@gmail.com>
2021-11-17 23:19:45 -0500

    patches on 1.0

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blubber_orm',
-    version='1.0.0',
+    version='1.0.1',
     description='Custom ORM for Hubbub DB',
     url='https://github.com/hubbub-tech/blubber-orm',
     author='Ade Balogun',

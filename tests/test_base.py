8a2e83a271a8a41a385508a4cbf0ed0cb93f9438
Ade Balogun <abalogun316@gmail.com>
2022-08-03 01:16:57 -0400

    missed things

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,71 +1,116 @@
-#import pytest
+import os
 import unittest
-import src.blubber_orm.base as base
 
-class TestAbstractModels(unittest.TestCase):
+import blubber_orm._conn as Blubber
+import blubber_orm._base as Models
 
-    def test_abstract_attributes(self):
-        #TODO: test that table_name and database exist in namespace
-        pass
+TABLE_NAME = os.getenv("TEST_TABLE_NAME")
+TABLE_PRIMARIES = ["id"]
 
-    def test_database_connection(self):
-        #TODO: get the class AbstractModels and make sure database is of type
-        #DatabaseConnection.
-        pass
 
-    def test_abstract_methods(self):
-        #TODO: try to instantiate the class, it should fail.
-        #Try to run functions, should similarly fail.
-        pass
+class FakeModels(Models):
+    """A fake table to test Blubber's Models Class."""
+
+    table_name = TABLE_NAME
+    table_primaries = TABLE_PRIMARIES
+    table_attributes = None
+
+    def __init__(self, attrs):
+        self.id = attrs["id"]
+        self.name = attrs["name"]
+
+
 
 class TestModels(unittest.TestCase):
 
-    def prepare_for_tests(self):
-        #TODO: create a new table for test attributes? or create a test db?
-        pass
-
-    def test_abstract_inheritance(self):
-        #TODO: test Models is of type AbstractModels. then make sure that all
-        #the methods defined in AbstractModels have been defined in Models. also
-        pass
-
     def test_insert(self):
-        #TODO: test data insertion works and handles errors as expected
-        #also rotate through table names to see if any of them fail
-        pass
+        # Test data insertion works and handles errors as expected
 
-    def test_set(self):
-        #TODO: test data updates and handles errors if there is an non-attribute key 'test'
-        #also rotate through table names to see if any of them fail
-        pass
+        # If it fails, it will fail silently and return 'None'
+        fake_model = FakeModels.insert({"id": 1, "name": "Pennywise the Dancing Clown"})
+        self.assertTrue(isinstance(fake_model, FakeModels))
+
+
 
     def test_get(self):
-        #TODO: test that function throws error at non-attributes or non-specific keys
-        #with appropriate errors. also rotate through table names to see if any of them fail
-        pass
+        #Test that function throws error at non-attributes or non-specific keys
+
+        pkeys_of_correct_type = {"id": 1}
+
+        with self.assertRaises(AssertionError):
+            pkeys_of_incorrect_type = 1
+            fake_model = FakeModels.get(pkeys_of_incorrect_type)
+
+        fake_model = FakeModels.get(pkeys_of_correct_type)
+        self.assertTrue(isinstance(fake_model, FakeModels))
+
+        pkeys_does_not_exist = {"id": 10000}
+        fake_model = FakeModels.get(pkeys_does_not_exist)
+        self.assertTrue(fake_model is None)
+
+
+    def test_set(self):
+        #Test data updates and handles errors if there is an non-attribute key 'test'
+
+        pkeys_of_correct_type = {"id": 1}
+
+        self.test_get()
+        fake_model = FakeModels.get(pkeys_of_correct_type)
+        self.assertTrue(isinstance(fake_model, FakeModels))
+
+        with self.assertRaises(AssertionError):
+            pkeys_of_incorrect_type = 1
+            FakeModels.set(pkeys_of_incorrect_type, {"name": "Ronald McDonald"})
+
+
+        FakeModels.set(pkeys_of_correct_type, {"name": "Ronald McDonald Jr."})
+        fake_model = FakeModels.get(pkeys_of_correct_type)
+
+        self.assertTrue(fake_model.name == "Ronald McDonald Jr.")
+
 
     def test_delete(self):
-        #TODO: delete a data row then check to make sure it's deleted by calling it back
-        #also rotate through table names to see if any of them fail
-        pass
+        # Delete a data row then check to make sure it's deleted by calling it back
+        pkeys = {"id": 1}
+
+        self.test_get()
+        fake_model = FakeModels.get(pkeys)
+        self.assertTrue(isinstance(fake_model, FakeModels))
+
+        FakeModels.delete(pkeys)
+
+        fake_model = FakeModels.get(pkeys)
+        self.assertTrue(fake_model is None)
+
+        FakeModels.insert({"id": 1, "name": "Pennywise the Dancing Clown"})
+
 
     def test_filter(self):
-        #TODO: test filter does not accept non-attributes. test it always returns type list
-        #also rotate through table names to see if any of them fail
-        pass
+        #Test filter does not accept non-attributes. test it always returns type list
 
-    def test_get_columns(self):
-        #TODO: test to be sure get_columns gets the correct columns for all the
-        #models, this is important to verification at every other class method
-        #also rotate through table names to see if any of them fail
-        pass
+        keys_do_exist = {"id": 1}
 
-    def test_refresh(self):
-        #TODO: create two instances of a model class with the same data
-        #then make a change to one and refresh it
-        #assert that the two instances are different
-        #also rotate through table names to see if any of them fail
-        pass
+        fake_models = FakeModels.filter(keys_do_exist)
+        self.assertTrue(isinstance(fake_models, list))
+
+        keys_do_not_exist = {"name": "The Joker"}
+        fake_models = FakeModels.filter(keys_do_not_exist)
+        self.assertTrue(isinstance(fake_models, list))
+        self.assertTrue(fake_models == [])
+
 
 if __name__ == '__main__':
+
+    with Blubber.conn.cursor() as cursor:
+        cursor.execute(f"""
+            CREATE TABLE {TABLE_NAME} (
+                id int,
+                name text,
+                PRIMARY KEY (id)
+            );
+        """)
+
     unittest.main()
+
+    with Blubber.conn.cursor() as cursor:
+        cursor.execute(f"DROP TABLE {TABLE_NAME};")
--- tests/test_conn.py
+++ tests/test_conn.py
@@ -1,51 +1,71 @@
-#import pytest
 import unittest
-import src.blubber_orm.db as db
+from blubber_orm._conn import Blubber
 
-class TestGlobalFunctions(unittest.TestCase):
 
-    def test_sql_to_dictionary(self):
-        #TODO: im not sure how to test this, but should always return type dict
-        #where key=db_column_name and value=db_corresponding_value
+class TestBlubber(unittest.TestCase):
+
+    def test_format_to_dict(self):
+        # TODO: im not sure how to test this, but should always return type dict
+        # where key=db_column_name and value=db_corresponding_value
         pass
 
     def test_parse_uri(self):
-        #TODO: should always correctly parse database uri, given conventional
-        #formatting, and should throw correct error if not.
+        # TODO: should always correctly parse database uri, given conventional
+        # formatting, and should throw correct error if not.
         pass
 
-class TestDatabaseConnection(unittest.TestCase):
 
     def test_singleton_before_connection(self):
-        #TODO: test that no instance exists and that the class contains:
-        #_instance, cursor, connection
-        pass
+        # Test that no instance exists and that the class contains:
+        self.assertEqual(Blubber._instance, None)
+
 
     def test_singleton_after_connection(self):
-        #TODO: test that instance exists and that the class cannot be
-        #initialized again. cursor and connection should also exist
-        pass
+        # Test that instance exists and that the class cannot be
+        test_conn = Blubber.get_instance()
+
+        # NOTE: Eventually, create a specific error type to watch for...
+        with self.assertRaises(Exception):
+            test_another_conn = Blubber()
+
+        # NOTE: Might need to write a way for these two to be compared...
+        test_another_conn = Blubber.get_instance()
+        self.assertEqual(test_conn, test_another_conn)
+
 
     def test_get_instance(self):
-        #TODO: should return object of type 'DatabaseConnection' containing not NULL
-        #_instance, cursor, connection
-        pass
+        # Should return object of type 'Blubber' containing not NULL
 
-    def test_get_db(self):
-        #TODO: given valid credentials, should return a tuple of
-        #cursor in position 0 and connection in position 1
-        pass
+        test_blubber = Blubber.get_instance()
+        self.assertTrue(isinstance(test_blubber, Blubber))
 
-    def test_close_db(self):
-        #TODO: should delete the singleton. singleton should be able to re-init after
-        #maybe also find a way to see if the database connection is actually closed
-        pass
 
-    def test_db_connection():
-        #connection_uri = "postgresql://adekunlebalogun:none@localhost:5432/adekunlebalogun"
-        #db_instance = DatabaseConnection.get_instance()
-        #print("Connection", db_instance)
-        pass
+    def test_open_conn(self):
+        # Sould return a connection object, defined by psycopg2
+
+        test_blubber = Blubber.get_instance()
+        test_conn = test_blubber.conn
+
+        # 1. Test the connections ability to execute query.
+        with test_conn.cursor() as cursor:
+            cursor.execute("CREATE TABLE test_connections;")
+            cursor.execute("DROP TABLE test_connections;")
+            test_conn.commit()
+
+        # TODO: 2. import Connection Class from psycopg2 and test that test_conn is an isntance of it
+
+
+    def test_close_conn(self):
+        # Should not delete the singleton. Should only NULL the .conn attribute.
+
+        test_blubber = Blubber.get_instance()
+        self.assertFalse(test_blubber.conn is None)
+
+        test_blubber.close_conn() # should test_blubber
+
+        self.assertTrue(test_blubber.conn is None)
+        self.test_singleton_after_connection()
+
 
 if __name__ == '__main__':
     unittest.main()
